9e7fa7c6fe167a2cb6da8eced3e30962b317af9e
James <james@rubify.com>
2014-05-26 00:57:08 +0800

    remove error string

--- Source/ObjectiveCDMDownloadTask.m
+++ Source/ObjectiveCDMDownloadTask.m
@@ -171,6 +171,7 @@ andTotalBytesExepectedToWrite:(int64_t)totalBytesExpectedToWriteInput
 - (NSString *) fullErrorDescription {
     if(self.error) {
         // return [NSString stringWithFormat:@"Downloading URL %@ failed because of error: %@ (Code %d)", self.urlString, [self.error localizedDescription], [self.error code]];
+        return @"";
     } else {
         return @"No Error";
     }//end else
